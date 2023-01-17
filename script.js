a7c18d06ca29508c9f6ef62402b37ab1261bc0d2
Bruno oliveira <brunogabriel2608@gmail.com>
2023-01-17 20:25:24 -0300

    feita logica de editar campo do cartao dinamicamente

--- script.js
+++ script.js
@@ -1,16 +1,50 @@
 const campos = document.querySelectorAll('input');
-let data = ''
 
 
 campos.forEach(item => {
     item.addEventListener('keyup', function(e) {
         
-        let edit = document.querySelector(`#${e.target.name}`);
-        let code = parseInt(e.keyCode);
+        let edit = document.querySelector(`.${e.target.name}`);
+        let code = e.keyCode;
+        let campoCard = edit.classList[0];
         
-        if((edit.id == 'name') && (code < 65 || code > 91) && (code != 8) ){
-            return
+        if((campoCard == 'name') && (code < 65 || code > 91) && (code != 8) ){
+            return;
         }
-        edit.innerText = e.target.value;
+
+        if((campoCard == 'number') && (e.target.value.replace(/ /g, '').length % 4 == 0) && !(isNaN(e.key))) {
+            if(e.target.value.replace(/ /g, '').length >= 16){
+                return;
+            }else{
+                e.target.value += ' ';
+            }
+            
+        }
+
+        if(e.target.value == ''){
+            switch(campoCard){
+                case 'number' :
+                    edit.innerText = '1234 5678 9123 4567';
+                    break;
+                case 'name' :
+                    edit.innerText = 'name last-name';
+                    break;
+                case 'mounth' :
+                    edit.innerText = '00';
+                    break;
+                case 'day' :
+                    edit.innerText = '00';
+                    break;
+                case 'cvc' :
+                    edit.innerText = '000';
+                    break;
+                default :
+                    return;
+            }
+                
+        }else{
+            edit.innerText = e.target.value.toUpperCase();
+        }
+        
     })
 })
