aa397961b5e3b4af06dc695af8acb693ba0b63c6
Bruno oliveira <brunogabriel2608@gmail.com>
2023-01-19 12:07:41 -0300

    feito integracao com o localStorage para salvar informacoes do formulario e preencher o card dinamicamente casoo tenha essas informacoes ja

--- script.js
+++ script.js
@@ -2,6 +2,21 @@ const campos = document.querySelectorAll('input');
 
 const btn = document.querySelector('#button-formCard');
 
+
+
+function preenche(){
+    const inf = JSON.parse(localStorage.getItem('info'));
+    
+    if(inf){
+        for (const [key, value] of Object.entries(inf)) {
+            const camp = document.querySelector(`.${key}`)
+            camp.innerText = value;
+          }
+        
+        
+    }
+}
+
 campos.forEach(item => {
     item.addEventListener('keyup', function(e) {
         
@@ -15,7 +30,7 @@ campos.forEach(item => {
 
         if((campoCard == 'number') && (e.target.value.replace(/ /g, '').length % 4 == 0) && !(isNaN(e.key))) {
             if(e.target.value.replace(/ /g, '').length >= 16){
-                return;
+                e.target.value += '';
             }else{
                 e.target.value += ' ';
             }
@@ -53,16 +68,36 @@ campos.forEach(item => {
 btn.addEventListener('click', (event) => {
     
     event.preventDefault();
+    let confere = 0;
     
     campos.forEach( e => {
         campo = e.validity;
         const mensagem = document.querySelector(`#span-${e.name}`);
         if(!(campo.valid)){        
-            
+            confere++;
             mensagem.innerText = `Preencha corretamente`
             
         }else{
             mensagem.innerText = ``
         }
     })
+    if(confere<1){
+        const form = document.querySelector(".form-card");
+        salva(form);
+    }
 })
+
+function salva(form){
+    const informationsCard = {
+        'name': form.querySelector("#name").value.toUpperCase(),
+        'number': form.querySelector("#number").value,
+        'mounth': form.querySelector("#mounth").value,
+        'day': form.querySelector("#day").value,
+        'cvc': form.querySelector("#cvc").value
+    };
+
+    localStorage.setItem('info', JSON.stringify(informationsCard));
+    form.reset();
+
+}
+preenche()
